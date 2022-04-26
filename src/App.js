f9ad155271b8ef86405c0889e42b64a44cbbe193
Luciana Zanetti <luciana.zeta@gmail.com>
2022-04-26 15:48:09 -0300

    sacando console

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Values from 'values.js';
 import ChromeColor from '@uiw/react-color-chrome';
 import { useEffect, useState } from "react";
-import { shape } from 'prop-types';
+
 
 
 function App() {
@@ -12,12 +12,12 @@ function App() {
   const [showPicker, setShowPicker] = useState(false);
 
   const onChange = (color) => {
-    console.log(color.hex);
+    //console.log(color.hex);
     setColor(color.hex)
     const colorValues = new Values(color.hex);
     const newColors = colorValues.all(10);
     newColors.pop();        //saca el ultimo color de los 20 q me trae
-    console.log(newColors);
+    //console.log(newColors);
     setShades(newColors);
 
   };
